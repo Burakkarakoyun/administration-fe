f9b89c0a49b91b569c6649ed217ff251b724a474
Burak Karakoyun <zihniburakkarakoyun@gmail.com>
2021-08-02 16:34:37 +0300

    frontend started!

---
+++ src/app/serverdown/serverdown.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ServerdownComponent } from './serverdown.component';
+
+describe('ServerdownComponent', () => {
+  let component: ServerdownComponent;
+  let fixture: ComponentFixture<ServerdownComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ServerdownComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ServerdownComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
